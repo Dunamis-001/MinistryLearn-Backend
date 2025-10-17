159e788b6bd8ca4f3df77163968fde7cd0871224
Dunamis-001 <alex.nyamai@student.moringaschool.com>
2025-10-17 21:59:01 +0300

    add UserRole model to manage user-role relationships with created_at timestam

---
+++ app/models/user_role.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+from ..extensions import db
+
+class UserRole(db.Model):
+    __tablename__ = "user_roles"
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), primary_key=True)
+    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"), primary_key=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
